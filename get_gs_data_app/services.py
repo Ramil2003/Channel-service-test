6e8fca1e2a1cd4ce764042af0e8e0333bbe1b22d
Ramil2003 <theluckyfeed1@gmail.com>
2022-07-15 18:19:30 +0300

    Add logics to services

--- get_gs_data_app/services.py
+++ get_gs_data_app/services.py
@@ -15,3 +15,21 @@ def authentication() -> Any:
     """
     return gspread.service_account(filename=settings.JSON_CONFIG)
 
+
+def get_data_from_gs() -> list[dict]:
+    """
+    Get data from Google Sheets (gs). \n
+    We authenticate user. \n
+    Open our page in Google Sheets. \n
+    Get and return all data from gs like list[dict].
+
+    :return: return all data from gs list[dict]
+    :rtype: list[dict]
+    """
+    auth = authentication()
+    sh = auth.open('Copy of test')
+    return sh.sheet1.get_all_records()
+
+
+def upload_data_to_db():
+    pass
