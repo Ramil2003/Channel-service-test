9bf88bc7105d42004c2c65b2aaee38ea95419daf
Ramil2003 <theluckyfeed1@gmail.com>
2022-07-17 17:41:03 +0300

    Upload data to db and check google sheets

--- get_gs_data_app/tasks.py
+++ get_gs_data_app/tasks.py
@@ -15,8 +15,15 @@ def upload_data_to_db() -> None:
     """
     data = _open_n_get_data_from_gs()
     dollar = _get_current_dollar()
+    qs1 = SheetsData.objects.all()
     for d in data:
         SheetsData.objects.update_or_create(order_num=d.get('заказ №', 0),
                                             cost_dol=d.get('стоимость,$', 0),
-                                            cost_rub=round(float(d.get('стоимость,$', 0) * dollar), 1),
+                                            cost_rub=round(float(d.get('стоимость,$', 0) * dollar), 1) or 0,
                                             delivery_time=d.get('срок поставки', "00.00.00"))
+        qs2 = SheetsData.objects.filter(order_num=d.get('заказ №', 0),
+                                        cost_dol=d.get('стоимость,$', 0),
+                                        cost_rub=round(float(d.get('стоимость,$', 0) * dollar), 1) or 0,
+                                        delivery_time=d.get('срок поставки', "00.00.00"))
+    if qs1.difference(qs2):
+        qs2.delete()
