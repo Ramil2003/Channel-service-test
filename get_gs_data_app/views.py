e470e1125535ba5ac9969087e6c11f94420475b9
Ramil2003 <theluckyfeed1@gmail.com>
2022-07-18 22:01:43 +0300

    Add get_context_data to get total

--- get_gs_data_app/views.py
+++ get_gs_data_app/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.db.models import Sum
 
 from .models import SheetsData
 
@@ -7,3 +8,8 @@ class DataListView(ListView):
     model = SheetsData
     context_object_name = 'data_list'
     template_name = 'index.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(DataListView, self).get_context_data()
+        context['total'] = SheetsData.objects.aggregate(Sum('cost_rub'))
+        return context
