470168187771058f491666bc186e5dbf1bb25862
Ramil2003 <theluckyfeed1@gmail.com>
2022-07-19 08:32:29 +0300

    Update docstring in get_current_dollar. Delete round() in tasks

--- get_gs_data_app/parsing_currency.py
+++ get_gs_data_app/parsing_currency.py
@@ -4,11 +4,11 @@ import xml.etree.ElementTree as ET
 from django.conf import settings
 
 
-def _get_current_dollar() -> float | int:
+def _get_current_dollar() -> float:
     """
     Get current dollar from XML file in https://www.cbr.ru/scripts/XML_daily.asp
 
-    :return: Current dollar
+    :return: Current dollar - float
     :rtype: float
     :except requests.exceptions.HTTPError: Returns error if something went wrong
     """
@@ -16,7 +16,7 @@ def _get_current_dollar() -> float | int:
         data = requests.get(settings.PARSING_SITE)
         if data.status_code == 200:
             dollar = ET.fromstring(data.text)
-            return float(dollar[10][4].text.replace(',', '.'))
+            return round(float(dollar[10][4].text.replace(',', '.')), 0)
         return 0
     except (requests.exceptions.HTTPError, TypeError, ValueError):
         return 0
