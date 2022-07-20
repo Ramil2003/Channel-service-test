6cf17a0aa4a29eeac1c07c85c2f400cf6cb5e6bd
Ramil2003 <theluckyfeed1@gmail.com>
2022-07-20 19:25:02 +0300

    Fix work with db

--- get_gs_data_app/tasks.py
+++ get_gs_data_app/tasks.py
@@ -17,16 +17,10 @@ def work_with_db() -> None:
     """
     data = _open_n_get_data_from_gs()
     dollar = _get_current_dollar()
-    qs_all = SheetsData.objects.all()
+    SheetsData.objects.all().delete()
     for d in data:
-        SheetsData.objects.update_or_create(order_num=d.get('заказ №', 0),
-                                            cost_dol=d.get('стоимость,$', 0),
-                                            cost_rub=d.get('стоимость,$', 0) * dollar,
-                                            delivery_time=d.get('срок поставки', "00.00.00"))
-        filtered_qs = SheetsData.objects.filter(order_num=d.get('заказ №', 0),
-                                                cost_dol=d.get('стоимость,$', 0),
-                                                cost_rub=d.get('стоимость,$', 0) * dollar,
-                                                delivery_time=d.get('срок поставки', "00.00.00"))
+        SheetsData.objects.create(order_num=d.get('заказ №', 0),
+                                  cost_dol=d.get('стоимость,$', 0),
+                                  cost_rub=d.get('стоимость,$', 0) * dollar,
+                                  delivery_time=d.get('срок поставки', "00.00.00"))
     data.clear()
-    if qs_all.difference(filtered_qs):
-        filtered_qs.delete()
